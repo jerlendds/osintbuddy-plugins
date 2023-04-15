d8ecaf283543ec8011b8dfbd5395e927c0c50855
jerlendds <jerlendds@proton.me>
2023-04-15 14:23:32 -0600

    chore: prototyping baseplugin

--- src/osintbuddy/plugins.py
+++ src/osintbuddy/plugins.py
@@ -1,14 +1,43 @@
-#   ---------------------------------------------------------------------------------
-#   Copyright (c) OSINTBuddy. All rights reserved.
-#   See LICENSE in project root for information.
-#   ---------------------------------------------------------------------------------
-"""This is a Sample Python file."""
-
-
 from __future__ import annotations
+import hashlib
+from typing import List
+from osintbuddy.utils import slugify
+from fastapi import APIRouter
+
+# @todo
 
 
-def osintbuddy(i: int = 0) -> str:
-    """Doc String."""
-    print("hello world")
-    return f"string-{i}"
+class BasePlugin(object):
+    def __init__(self) -> None:
+        self.transforms: dict = None
+        self.node: dict = None
+        self.node_name: str = None
+        self.settings: dict = None
+        self.repository: str = None
+        self.authors = List[str]
+        self._base_plugin_endpoint = slugify(self.node_name)
+        self._endpoints = self.generate_endpoints()
+        self._router: APIRouter = APIRouter(prefix=self._base_plugin_endpoint)
+
+    def request(self, transform: str, **kwargs):
+        return self.transforms[transform](**kwargs)
+
+    def _generate_endpoints(self):
+        if self.transforms is None:
+            return
+        endpoints = []
+        for transform in self.transforms.keys():
+            route = str.encode(
+                f"{self._base_plugin_endpoint}{transform}{self.repository}"
+            )
+            endpoint_route = hashlib.md5(route).hexdigest()
+            self._router.add_api_route(
+                endpoint_route,
+                endpoint=self.transforms[transform]
+            )
+        return endpoints
+
+
+class Plugin(BasePlugin):
+    def __init__(self):
+        pass
