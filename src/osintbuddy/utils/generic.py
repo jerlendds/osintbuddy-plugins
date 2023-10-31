4872fe2136744bb92b80f54bae9f36238ebfcc3c
jerlendds <jerlendds@osintbuddy.com>
2023-10-31 12:34:11 -0600

    Rework templates file to templates directory

--- src/osintbuddy/utils/generic.py
+++ src/osintbuddy/utils/generic.py
@@ -38,33 +38,6 @@ def to_clean_domain(value: str) -> str:
     return domain
 
 
-def plugin_source_template(label: str, description: str, author: str) -> str:
-    class_name = ''.join(x for x in filter(str.isalnum, label.title()) if not x.isspace())
-
-    return f"""import osintbuddy as ob
-from osintbuddy.elements import TextInput
-
-class {class_name}(ob.Plugin):
-    label = '{label}'
-    icon = 'atom'   # https://tabler-icons.io/
-    color = '#FFD166'
-
-    author = '{author}'
-    description = '{description}'
-
-    node = [
-        TextInput(label='Example', icon='radioactive')
-    ]
-
-    @ob.transform(label='To example', icon='atom')
-    async def transform_example(self, node, use):
-        WebsitePlugin = await ob.Registry.get_plugin('website')
-        website_plugin = WebsitePlugin()
-        return website_plugin.blueprint(domain=node.example)
-"""
-
-
-
 # Slugify and related code is from the Django project, thanks guys!
 # Project URL: https://github.com/django/django
 # https://github.com/django/django/blob/main/django/utils/text.py
